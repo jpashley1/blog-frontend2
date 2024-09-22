8cae1e64f294597eb660401f6ab2ffd13c229f1e
jpashley1 <jpashley1@gmail.com>
2024-09-22 16:31:37 -0400

    Added index function.

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,4 +1,13 @@
-export function PostsIndex()
-return (
-  
-)
+export function PostsIndex({ posts }) {
+  return (
+    <div id="PostsIndex">
+      <h1>All Posts</h1>
+       {posts.map((post) => (
+         <div key={post.id}>
+           <h2>{post.name}</h2>
+           <img src={post.image} />
+         </div>
+       ))}
+    </div>
+  );
+}
--- src/PostsPage.jsx
+++ src/PostsPage.jsx
@@ -1,7 +1,25 @@
-export function PostsPage() {
-  return (
-    <main>
-      <h1>Welcome to React!</h1>
-    </main>
-  )
-}
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { PostsIndex } from "./PostsIndex"
+
+
+  export function PostsPage() {
+
+   const [posts, setPosts] = useState([]);
+
+   const handleIndex = () => {
+     console.log("handleIndex");
+     axios.get("http://localhost:3000/posts.json").then((response) => {
+       console.log(response.data);
+       setPosts(response.data);
+     });
+   };
+
+   useEffect(handleIndex, []);
+
+    return (
+      <main>
+        <PostsIndex posts={posts} />
+      </main>
+    );
+  }
